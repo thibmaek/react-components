f28bf1c98c8cf62874af7b8c4001de1688ae41cf
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2018-10-08 00:46:37 +0200

    Add demo project

---
+++ react-native/demo/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import * as Examples from './examples';
+
+export default class App extends React.Component {
+  render() {
+    return null;
+  }
+}
